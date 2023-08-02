51deec9594429c49714f1a07df75be5d0e5e4ba8
nestor4513289 <128256276+nestor4513289@users.noreply.github.com>
2023-08-01 20:04:57 -0500

    Add files via upload

---
+++ codigo de las actividad de aprendizaje/ascii.py
@@ -0,0 +1,9 @@
+# Conversión de caracter a código ASCII
+caracter = input("introducir un caracter: ")
+codigo_ascii = ord(caracter)
+print(codigo_ascii)  # Output: 65
+
+# Conversión de código ASCII a caracter
+codigo_ascii = 66
+caracter = chr(codigo_ascii)
+print(caracter)  # Output: B
